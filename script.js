3740cbaf4541932437cd1b4e71c1cb29a7ef53e1
sam <samrjones03@gmail.com>
2022-06-18 17:39:42 -0400

    added rock paper scissors based on prompt

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ function computerPlay (rps) {
 const rps = ["rock", "paper", "scissors"];
 
 function playRound (playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a tie!"
+    }
+
     if (playerSelection === "rock") {
         if (computerSelection === "scissors")
         return "You Win! Rock beats Scissors!";
         if (computerSelection === "paper")
         return "You Lose! Paper beats Rock!";
-        if (computerSelection === "rock")
-        return "It's a tie!"
     }
 
     if (playerSelection === "paper") {
@@ -19,8 +21,6 @@ function playRound (playerSelection, computerSelection) {
         return "You Lose! Scissors beats Paper!";
         if (computerSelection === "rock")
         return "You Win! Paper beats Rock!";
-        if (computerSelection === "paper")
-        return "It's a tie!"
     }
 
     if (playerSelection === "scissors") {
@@ -28,13 +28,12 @@ function playRound (playerSelection, computerSelection) {
         return "It's a tie!";
         if (computerSelection === "rock")
         return "You Lose! Rock beats Scissors!";
-        if (computerSelection === "paper")
-        return "You Win! Scissors beats Paper!"
     }
 }
 
 function game() {
-    
+    for (let i = 0; i < 5; i++) {
+    }
 }
 
 const playerSelection = (prompt("Rock, paper, or scissors?")).toLowerCase();
