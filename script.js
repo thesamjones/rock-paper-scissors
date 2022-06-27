49b6539c01d5e1bd70d2f9eacb2d53fc4ca676ed
sam <samrjones03@gmail.com>
2022-06-27 18:38:29 -0400

    finished loop

--- script.js
+++ script.js
@@ -10,32 +10,54 @@ function playRound (playerSelection, computerSelection) {
     }
 
     if (playerSelection === "rock") {
-        if (computerSelection === "scissors")
+        if (computerSelection === "scissors") {
+            ++playerScore
         return "You Win! Rock beats Scissors!";
-        if (computerSelection === "paper")
+        } if (computerSelection === "paper") {
+            ++computerScore
         return "You Lose! Paper beats Rock!";
+        }
     }
 
     if (playerSelection === "paper") {
-        if (computerSelection === "scissors")
+        if (computerSelection === "scissors") {
+            ++computerScore
         return "You Lose! Scissors beats Paper!";
-        if (computerSelection === "rock")
+        } if (computerSelection === "rock") {
+            ++playerScore
         return "You Win! Paper beats Rock!";
+        }
     }
 
     if (playerSelection === "scissors") {
-        if (computerSelection === "rock")
+        if (computerSelection === "rock") {
+            ++computerScore
         return "You Lose! Rock beats Scissors!";
-        if (computerSelection === "paper")
+        } if (computerSelection === "paper") {
+            ++playerScore
         return "You Win! Scissors beats Paper!"
+        }
     }
 }
 
 function game() {
     for (let i = 0; i < 5; i++) {
+        const computerSelection = computerPlay(rps);
+        const playerSelection = (prompt("Rock, paper, or scissors?")).toLowerCase();
+        console.log(playRound (playerSelection, computerSelection));
+    }
+
+    if (playerScore > computerScore) {
+        return "You win!";
+    } if (computerScore > playerScore) {
+        return "You lose!";
+    } if (playerScore === computerScore) {
+        return "Stalemate!"
     }
 }
 
-const playerSelection = (prompt("Rock, paper, or scissors?")).toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
+
 const computerSelection = computerPlay(rps);
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
